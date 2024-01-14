000f4499a926558ec2e04ac94f9a8cdc596ac1e8
John Polacek <johnpolacek@veg.vet>
2024-01-14 14:48:55 -0600

    update README

--- app/ui/Logo.tsx
+++ app/ui/Logo.tsx
@@ -4,9 +4,9 @@ import FadeIn from '../animation/FadeIn';
 const Logo = () => (
   <div className="pointer-events-none flex justify-center w-full pt-4 pb-8">
     <div className="grid grid-cols-2 grid-rows-2 w-16 h-16 gap-1 -rotate-45 opacity-50">
-      <FadeIn className="border-[3px] border-purple-500 duration-1000" animateFromClassname="-rotate-180" animateToClassname="rotate-0"></FadeIn>
-      <FadeIn animateFromClassname="translate-x-6" animateToClassname="translate-x-0" delay={700} className="border-[3px] border-purple-500 duration-1000"></FadeIn>
-      <FadeIn animateFromClassname="translate-y-6" animateToClassname="translate-y-0" delay={900} className="border-[3px] border-purple-500 duration-700"></FadeIn>
+      <FadeIn className="border-[3px] border-purple-500 duration-1000" from="-rotate-180" to="rotate-0"></FadeIn>
+      <FadeIn from="translate-x-6" to="translate-x-0" delay={700} className="border-[3px] border-purple-500 duration-1000"></FadeIn>
+      <FadeIn from="translate-y-6" to="translate-y-0" delay={900} className="border-[3px] border-purple-500 duration-700"></FadeIn>
       <div></div>
     </div>
   </div>
